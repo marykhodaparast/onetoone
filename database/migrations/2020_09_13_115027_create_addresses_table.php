6c42075a5cdbe96798a4101e243dfaac65b90bc9
maryam khodaparast <khcomputergirl@gmail.com>
2020-09-13 16:31:44 +0430

    Setting up Relations

--- database/migrations/2020_09_13_115027_create_addresses_table.php
+++ database/migrations/2020_09_13_115027_create_addresses_table.php
@@ -16,6 +16,7 @@ class CreateAddressesTable extends Migration
         Schema::create('addresses', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->integer('user_id')->unisigned()->nullable();
             $table->timestamps();
         });
     }
